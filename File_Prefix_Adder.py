5f86bdb1bf0434b11432232863069e745d324b35
N4YuC4 <yucelcan028@gmail.com>
2023-08-05 00:42:25 +0300

    comment line added

--- File_Prefix_Adder.py
+++ File_Prefix_Adder.py
@@ -1,14 +1,21 @@
 import datetime,os
-count=1
+
+count=1 #index numaraları için sayma sayacı
+
 path=input("(DİKKAT BU BELİRTİLEN YOL YANLIŞ GİRİLDİĞİ TAKDİRDE İSTENMEYECEK SONUÇLARA YOL AÇABİLİR LÜTFEN İŞLEMİN SAĞLANACAĞI KLASOR YOLUNU BİR KEZ DAHA KONTROL EDEREK GİRİNİZ)\nKlasör yolunu giriniz:")
-os.chdir(path)
-bulunanlar=os.listdir()
+os.chdir(path) #belirtilen dizine gidilir
+
+bulunanlar=os.listdir() #dizinde bulunanların listesi
+
+#dizinde bulunanları değeri oluşturulma tarihi olucak sekilde bir sozluk oluşturulur
 tarih_ile_bulunanlar={}
 for i in bulunanlar:
     tarih_ile_bulunanlar[i]=datetime.datetime.strftime(datetime.datetime.fromtimestamp(os.stat(f"{i}").st_ctime),"%c")
 
-sorted_list=sorted(tarih_ile_bulunanlar.items(), key=lambda x:x[1])
-sorted_dict=dict(sorted_list)
-for i in sorted_dict.keys():
+sorted_list=dict(sorted(tarih_ile_bulunanlar.items(), key=lambda x:x[1]))#olusturlan sozluk tarihe gore sıralanır
+
+
+#sozlukteki her bir elemana prefix numaraları yerleştirilir
+for i in sorted_list.keys():
     os.rename(i,f"{count}-{i}")
     count+=1
